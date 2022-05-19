866ca685442c37528987c786700f303d41232a57
Alain Tresor <tresoralain35@gmail.com>
2022-05-19 19:28:18 +0200

    add form validation

--- javascript.js
+++ javascript.js
@@ -60,3 +60,32 @@ window.onscroll = function () {
         myNav.classList.remove("nav-colored");
     }
 };
+
+
+
+
+const form = document.getElementById("messageForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const userMsg = document.getElementById("message");
+
+const errorMsg = document.getElementsByClassName("error");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  validate(username, 0, "Name cannot be blank");
+  validate(email, 1, "Please enter your email address");
+  validate(userMsg, 2, "Message cannot be blank");
+});
+
+let validate = (id, serial, message) => {
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+  }
+};
+
